0be277a7e106f089bcacf42cf599cdccdabf5f45
aditya Rahatekar <adityatech04@gmail.com>
2016-09-14 13:12:15 +0530

    lines --

--- calss1.java
+++ calss1.java
@@ -3,7 +3,7 @@ package project1;
 public class calss1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		System.out.println("hello_to_all_i_am_here!");
 		int initialBalance = 10000 ;
